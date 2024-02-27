87b0c701e2184b95bb98004771fc82768bc81457
Marcos <msoto@hopewellsystem.com>
2024-02-26 19:19:43 -0600

    m4 cons t5 1

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+    	if(year % 4 ==0 && year % 100 !=0 || year % 400 == 0) {
+    		System.out.println("leap");
+    	} else {
+    		System.out.println("not leap");
+    	}
+    	
+    }
+    public static void main(String[] args) {
+    	LeapYearPrinter leap = new LeapYearPrinter();
+    	leap.isLeapYear(2024);
     }
 }
+
+// Un año es bisiesto si es divisible por 4 pero no por 100, o si es divisible por 400.
